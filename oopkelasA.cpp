2a91f8829e7fe64d7ebe28252352f7248bc745fd
tiarahasnabila <tiarahasnabila@gmail.com>
2023-04-12 14:31:09 +0700

    Add project files.

---
+++ oopkelasA.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+
+private:
+	int nim;
+	string nama;
+public :
+	void inputdata() {
+		cout << "Masukkan NIM : ";
+		cin >> nim;
+		cout << "Masukkan Nama : ";
+		cin >> nama;
+	}
+	void tampildata() {
+		cout <<n
+	}
+}
